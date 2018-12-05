13fd483bfaedc457ad0fae69e6cd65b4fc4dbea4
sybek96 <sybek96@interia.pl>
2018-12-05 12:47:36 +0000

    added a break in a switch

--- NetworkingProject/Tutorial 7/Tutorial 7/Tut 7 - Client/Tut 7 - Client/Client.cpp
+++ NetworkingProject/Tutorial 7/Tutorial 7/Tut 7 - Client/Tut 7 - Client/Client.cpp
@@ -29,6 +29,7 @@ bool Client::ProcessPacket(Packet _packettype)
 	case P_Authoritative: //If packet is authoritative type
 	{
 		game->setAuthorative(true);
+		break;
 	}
 	default: //If packet type is not accounted for
 		std::cout << "Unrecognized packet: " << _packettype << std::endl; //Display that packet was not found
